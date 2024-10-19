09d523e1a9f82e0325bb24da20d57f9d583b18c6
kirill pavlov <2020kirillp@gmail.com>
2024-10-18 23:14:57 -0700

    Fetching wors

--- src/app/book/[title]/page.tsx
+++ src/app/book/[title]/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
 import { ArrowUpRight } from "lucide-react";
+import { Dot } from "lucide-react";
 
 export default async ({ params }: { params: { title: string } }) => {
 	const supabase = createClient();
@@ -24,17 +25,25 @@ export default async ({ params }: { params: { title: string } }) => {
 				<h2 className="uppercase text-center">{book.title}</h2>
 			</div>
 			<div className="flex flex-row h-screen relative">
-				<div className="w-1/2 border-r-black border-r"></div>
+				<div className="w-1/2 border-r-black border-r">
+					<p className="flex flex-row items-center font-semibold w-1/2">
+						<Dot />
+						{book.quote}
+					</p>
+					<p className="w-1/2 ml-28">{book.description}</p>
+				</div>
 				<img
 					src={book.cover}
 					alt=""
 					className="h-[500px] absolute shadow-2xl top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
 				/>
 				<div className="w-1/2 bg-slate-50">
-					<h3>{book.title}</h3>
-					<Link href={book.goodreads}>
-						Goodreads <ArrowUpRight />{" "}
-					</Link>
+					<div className="flex flex-col items-start mt-[250px] ml-52">
+						<h3 className="font-bold">{book.title}</h3>
+						<a href={book.goodreads} className="flex flex-row items-center">
+							Goodreads <ArrowUpRight />{" "}
+						</a>
+					</div>
 				</div>
 			</div>
 		</>
